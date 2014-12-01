1cc95eac9c606147274187bb5f8c529d70c243ca
Lex Alexander <l.alexander10@gmail.com>
2014-12-01 09:44:56 -0800

    refactored main scraper code

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,23 @@
 require "rake"
 
-task :default do
-	Rake::FileList.new("**/*rb").each do |file|
-		ruby file 
+task default: %w[getData]
+
+task :getData do 
+	Rake::FileList.new("Atlanta/rakefile.rb").each do |scraper_script|
+		dir = File.dirname(__FILE__)
+		city = File.basename(dir)
+		csv_file = File.join(dir, "resultsFor#{city}.csv")
+		ruby [scraper_script, csv_file].join(" ")
+		## This returns everything but the filename or last parameter
+		## passed in, so in this case it would give me Atlanta
+		## This returns the last componet of the filename and in this 
+		## case it will be the city 
+	## Calling join on file right here basically turns this into a path
+
+
+end 
 end
-end
+
+
+
+
